f7faf87bce2c651602ff881fc54dde0d9612ff01
alonsocopete@outlook.es <alonsocopete@outlook.es>
2026-09-22 22:26:34 +0000

    Updated MODEL_ENDPOINT

--- agent_server/config.py
+++ agent_server/config.py
@@ -2,4 +2,4 @@ import os
 
 CATALOG = os.getenv("CATALOG", "secop_ai")
 SECOP_AI_TOOLS_SCHEMA = os.getenv("SECOP_AI_TOOLS_SCHEMA", "ai_agents_tools")
-MODEL_ENDPOINT = os.getenv("MODEL_ENDPOINT", "databricks-gpt-oss-120b")
+MODEL_ENDPOINT = os.getenv("MODEL_ENDPOINT", "databricks-meta-llama-3-3-70b-instruct")
